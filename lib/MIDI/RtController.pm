4725a3ef44cda39d11802bec7245fc2ffc8614dc
Gene Boggs <gene@ology.net>
2025-03-10 11:18:02 -0700

    Fix

--- lib/MIDI/RtController.pm
+++ lib/MIDI/RtController.pm
@@ -198,7 +198,7 @@ sub _rtmidi_loop ($msg_ch, $midi_ch) {
 }
 
 sub _filter_and_forward ($self, $dt, $event) {
-    my $event_filters = $self->filters->{ 'all' } // [];
+    my $event_filters = $self->filters->{all} // [];
     push @{ $event_filters }, @{ $self->filters->{ $event->[0] } // [] };
 
     for my $filter ($event_filters->@*) {
