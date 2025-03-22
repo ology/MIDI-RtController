97866c1a7f8a1d1fa7b91d2ca1bb3bf7ad3956e6
Gene Boggs <gene@ology.net>
2025-03-22 14:12:21 -0700

    Tidy

--- eg/echo.pl
+++ eg/echo.pl
@@ -13,7 +13,7 @@ my $rtc = MIDI::RtController->new( input => $in, output => $out );
 $rtc->add_filter(
     'echo',
     all => sub {
-        my ($port, $dt, $event) = @_;
+        my ( $port, $dt, $event ) = @_;
         print "port: $port, dt: $dt, ev: ", join( ', ', @$event ), "\n"
             unless $event->[0] eq 'clock';
         return 0;
