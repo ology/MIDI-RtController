601e0bace0da0e4dfff69999d855316d232c4bd3
Gene Boggs <gene@ology.net>
2025-03-22 14:12:55 -0700

    Port

--- eg/tester.pl
+++ eg/tester.pl
@@ -69,7 +69,7 @@ sub add_filters ($name, $coderef) {
 sub pedal_notes ($note) {
     return PEDAL, $note, $note + 7;
 }
-sub pedal_tone ($dt, $event) {
+sub pedal_tone ($port, $dt, $event) {
     my ($ev, $chan, $note, $vel) = $event->@*;
     my @notes = pedal_notes($note);
     my $delay_time = 0;
@@ -83,7 +83,7 @@ sub pedal_tone ($dt, $event) {
 sub delay_notes ($note) {
     return ($note) x $feedback;
 }
-sub delay_tone ($dt, $event) {
+sub delay_tone ($port, $dt, $event) {
     my ($ev, $chan, $note, $vel) = $event->@*;
     my @notes = delay_notes($note);
     my $delay_time = 0;
@@ -112,7 +112,7 @@ sub drum_parts ($note) {
     }
     return $part;
 }
-sub drums ($dt, $event) {
+sub drums ($port, $dt, $event) {
     my ($ev, $chan, $note, $vel) = $event->@*;
     return 1 unless $ev eq 'note_on';
     my $part = drum_parts($note);
