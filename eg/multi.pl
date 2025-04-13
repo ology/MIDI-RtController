e9912b98b5232b194f38cb0838f7894cf31b5974
Gene Boggs <gene@ology.net>
2025-04-13 00:22:24 -0700

    Generic!

--- eg/multi.pl
+++ eg/multi.pl
@@ -7,21 +7,24 @@ use v5.36;
 
 use MIDI::RtController ();
 
-my $input_name_1 = shift || 'joystick'; # midi controller device
-my $input_name_2 = shift || 'tempopad'; # midi controller device
-my $output_name  = shift || 'fluid';    # fluidsynth output
+my $input_names = shift || 'joystick,tempopad'; # midi controller devices
+my $output_name = shift || 'fluid';    # fluidsynth output
 
-my $rtc_1 = MIDI::RtController->new(
-    input   => $input_name_1,
+my $inputs = [ split /,/, $input_names ];
+
+my $control = MIDI::RtController->new(
+    input   => $inputs->[0],
     output  => $output_name,
     verbose => 1,
 );
 
-my $rtc_2 = MIDI::RtController->new(
-    input    => $input_name_2,
-    loop     => $rtc_1->loop,
-    midi_out => $rtc_1->midi_out,
-    verbose  => 1,
-);
+for my $name (@$inputs[1 .. $#$inputs]) {
+    MIDI::RtController->new(
+        input    => $name,
+        loop     => $control->loop,
+        midi_out => $control->midi_out,
+        verbose  => 1,
+    );
+}
 
-$rtc_2->run;
+$control->run;
