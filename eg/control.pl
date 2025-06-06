13361ccb0d144dba21b9e7d17b9d04fdeeb59d7f
Gene Boggs <gene@ology.net>
2025-06-05 18:20:36 -0700

    Fix

--- eg/control.pl
+++ eg/control.pl
@@ -4,17 +4,14 @@ use warnings;
 
 use MIDI::RtController ();
 
-# but is required by the module, nonetheless.
 my $in  = shift || 'pad'; # Synido TempoPAD Z-1
-# output MIDI device
 my $out = shift || 'usb'; # USB MIDI Interface
 
-# set-up the controller
-my $rtc = MIDI::RtController->new(
+my $controller = MIDI::RtController->new(
     input   => $in,
     output  => $out,
     verbose => 1,
 );
 
 # set the controls for the heart of the sun
-$rtc->run;
+$controller->run;
